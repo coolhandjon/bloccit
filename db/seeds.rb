96bc9e5f0cd2a614949c07efb7af1d17f18452bb
Jonathan Yu <jonyu7@me.com>
2016-06-30 23:45:14 -0400

    assignment-33-bloccit: incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,6 +40,16 @@ Comment.find_or_create_by!(
   )
 end
 
+5.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
+puts "#{Question.count} questions created"
